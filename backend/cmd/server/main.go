971bf1e9d9c1e431c2a72f6cd51a0a254dac714c
Michal Redmerski <mr@zaaps.pl>
2026-07-14 10:00:39 +0200

    superinitial

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"log"
 
-	"github.com/redmeros/volley/backend/internal/config"
+	"github.com/redmeros/volley/internal/config"
 )
 
 func main() {
-	configPath := "../../config.toml"
+	configPath := "./config.toml"
 	_, err := config.ReadConfig(configPath)
 	if err != nil {
 		log.Fatalf("Failed to read config: %v", err)
